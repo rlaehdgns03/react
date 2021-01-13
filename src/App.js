a47dcbae39c4335eaffe8abb1c56c1417524bade
rlaehdgns03 <rlaehdgns03@gmail.com>
2021-01-13 15:44:32 +0900

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import React, { Component } from "react";
+import Subject from "./components/Subject";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
 import "./style.css";
 
-class Subject extends Component{
-  render(){
-    return(
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component{
-  render(){
-    return(
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component{
-  render(){
-    return(
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
   render() {
     return(
       <div className="App">
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <Subject 
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
