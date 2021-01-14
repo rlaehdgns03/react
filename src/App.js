82bf2deb0a5ea61fa313f6c4103d9af2a70ed5d4
rlaehdgns03 <rlaehdgns03@gmail.com>
2021-01-14 13:39:02 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is ...'},
         {id:2, title:'CSS', desc:'CSS is ...'},
@@ -17,14 +19,25 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return(
       <div className="App">
-        <Subject 
-          title={this.state.subject.title}
-          sub={this.state.subject.sub}>
-        </Subject>
+        <header>
+          <h1><a href='/' onClick={function(e){
+            alert('hi');
+            e.preventDefault(); // 페이지 리로드 방지
+          }}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     ) ;
   }
