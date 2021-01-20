46d1ebb37e150435b33d55db5b2e6297c134afca
rlaehdgns03 <rlaehdgns03@gmail.com>
2021-01-20 15:23:14 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "./style.css";
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode:'read',
       selected_content_id:2,
@@ -41,7 +42,13 @@ class App extends Component {
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
-        
+        this.max_content_id = this.max_content_id+1;
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:_contents
+        });
       }.bind(this)}></CreateContent>
     }
     return(
