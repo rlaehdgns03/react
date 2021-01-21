fa81bde9d4f09e3c3a1351ee2db9a6a887d54f5c
rlaehdgns03 <rlaehdgns03@gmail.com>
2021-01-21 17:19:57 +0900

    2 files modified

--- src/App.js
+++ src/App.js
@@ -55,8 +55,16 @@ class App extends Component {
       }.bind(this)}></CreateContent>
     }else if(this.state.mode === 'update'){
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
-        this.max_content_id = this.max_content_id+1;
+      _article = <UpdateContent data={_content} onSubmit={function(_id,_title, _desc){
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id,title:_title, desc:_desc};
+            break;
+          }
+          i = i + 1;
+        }
         var _contents = this.state.contents.concat(
           {id:this.max_content_id, title:_title, desc:_desc}
         )
--- src/components/UpdateContent.js
+++ src/components/UpdateContent.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 class UpdateContent extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      title:this.props.data.title,
+      desc:this.props.data.desc
+    }
+    this.inputFormHandler = this.inputFormHandler.bind(this);
+  }
+  inputFormHandler(e){
+    this.setState({[e.target.name]:e.target.value});
+  }
   render(){
     return(
       <article>
@@ -8,13 +19,31 @@ class UpdateContent extends Component{
         <form action="/create_process" method="post" onSubmit={function(e){
           e.preventDefault();
           this.props.onSubmit(
-            e.target.title.value,
-            e.target.desc.value
+            this.state.id,
+            this.state.title,
+            this.state.desc
           );
-        }.bind(this)}>
-          <p><input type="text" name="title" placeholder="title"></input></p>
-          <p><textarea name="desc" placeholder="description"></textarea></p>
-          <p><input type="submit"></input></p>
+        }.bind(this)}
+      >
+        <input type="hidden" name="id" value={this.state.id}></input>
+        <p>
+          <input 
+            type="text" 
+            name="title" 
+            placeholder="title" 
+            value={this.state.title} 
+            onChange={this.inputFormHandler}>
+          </input>
+        </p>
+        <p>
+          <textarea 
+            name="desc" 
+            placeholder="description" 
+            value={this.state.desc} 
+            onChange={this.inputFormHandler}>
+          </textarea>
+        </p>
+        <p><input type="submit"></input></p>
         </form>
       </article>
     );
