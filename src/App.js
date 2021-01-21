14b61f462f4809626cadf74918d317f28d59ea01
rlaehdgns03 <rlaehdgns03@gmail.com>
2021-01-21 15:48:18 +0900

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Subject from "./components/Subject";
 import TOC from "./components/TOC";
 import ReadContent from "./components/ReadContent";
 import CreateContent from "./components/CreateContent";
+import UpdateContent from "./components/UpdateContent";
 import Control from "./components/Control";
 import "./style.css";
 
@@ -22,24 +23,26 @@ class App extends Component {
       ]
     }
   }
-  render() {
+  getReadContent(){
+    var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          return data;
+          break;
+        }
+        i = i + 1;
+      }
+  }
+  getContent(){
     var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     }else if(this.state.mode === 'read'){
-      var i = 0;
-      while(i < this.state.contents.length){
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id){
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i = i + 1;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     }else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title, _desc){
         this.max_content_id = this.max_content_id+1;
@@ -50,7 +53,21 @@ class App extends Component {
           contents:_contents
         });
       }.bind(this)}></CreateContent>
+    }else if(this.state.mode === 'update'){
+      _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc){
+        this.max_content_id = this.max_content_id+1;
+        var _contents = this.state.contents.concat(
+          {id:this.max_content_id, title:_title, desc:_desc}
+        )
+        this.setState({
+          contents:_contents
+        });
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
     return(
       <div className="App">
       <Subject
@@ -75,7 +92,7 @@ class App extends Component {
           mode:_mode
         });
       }.bind(this)}></Control>
-      {_article}
+      {this.getContent()}
       </div>
     ) ;
   }
